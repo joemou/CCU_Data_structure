313631437fbeab0122cc64bf75b25a4201e80804
JM <60748971+joemou@users.noreply.github.com>
2023-12-28 00:04:00 +0800

    update

--- teach/teachhw3.c
+++ teach/teachhw3.c
@@ -1,11 +1,9 @@
-// BFS algorithm in C
-
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 10000
 
 struct queue {
-  int items[SIZE];
+  int* items;
   int front;
   int rear;
 };
@@ -13,9 +11,7 @@ struct queue {
 struct queue* createQueue();
 void enqueue(struct queue* q, int);
 int dequeue(struct queue* q);
-void display(struct queue* q);
 int isEmpty(struct queue* q);
-void printQueue(struct queue* q);
 
 struct node {
   int vertex;
@@ -28,10 +24,15 @@ struct Graph {
   int numVertices;
   struct node** adjLists;
   int* visited;
+  int* parent;
 };
 
 // BFS algorithm
-void bfs(struct Graph* graph, int startVertex) {
+void bfs(struct Graph* graph, int startVertex, int endVertex, int*** paths, int* pathSizes) {
+  for (int i = 0; i < graph->numVertices; i++) {
+    graph->parent[i] = -1; // Initialize parent array to -1
+  }
+
   struct queue* q = createQueue();
 
   graph->visited[startVertex] = 1;
@@ -39,7 +40,53 @@ void bfs(struct Graph* graph, int startVertex) {
 
   while (!isEmpty(q)) {
     int currentVertex = dequeue(q);
-    printf("Visited %d\n", currentVertex);
+
+    if (currentVertex == endVertex) {
+      // Found the end node, store the path
+      int v = endVertex;
+      int pathIndex = *pathSizes;
+      int pathSize = 1;
+
+      // Traverse the parent pointers to determine the path size
+      while (v != startVertex) {
+        v = graph->parent[v];
+        pathSize++;
+      }
+
+      // Allocate memory for the path
+      (*paths) = realloc(*paths, (*pathSizes + 1) * sizeof(int*));
+      (*paths)[*pathSizes] = malloc((graph->numVertices + 1) * sizeof(int));  // +1 for the sentinel value -1
+
+      int index = 0;
+      v = endVertex;
+
+      // Store the path in forward order
+      while (v != -1) {
+          (*paths)[*pathSizes][index++] = v;
+          v = graph->parent[v];
+      }
+
+      // Add sentinel value -1 at the end of the path
+      (*paths)[*pathSizes][index] = -1;
+
+      // Reverse the path in-place
+      int start = 0, end = index - 1;
+      while (start < end) {
+          int temp = (*paths)[*pathSizes][start];
+          (*paths)[*pathSizes][start] = (*paths)[*pathSizes][end];
+          (*paths)[*pathSizes][end] = temp;
+          start++;
+          end--;
+      }
+
+
+      // Add sentinel value -1 at the end of the path
+      (*paths)[*pathSizes][index] = -1;
+
+      // Update pathSizes
+      (*pathSizes)++;
+
+    }
 
     struct node* temp = graph->adjLists[currentVertex];
 
@@ -48,6 +95,7 @@ void bfs(struct Graph* graph, int startVertex) {
 
       if (graph->visited[adjVertex] == 0) {
         graph->visited[adjVertex] = 1;
+        graph->parent[adjVertex] = currentVertex;
         enqueue(q, adjVertex);
       }
       temp = temp->next;
@@ -70,11 +118,12 @@ struct Graph* createGraph(int vertices) {
 
   graph->adjLists = malloc(vertices * sizeof(struct node*));
   graph->visited = malloc(vertices * sizeof(int));
+  graph->parent = malloc(vertices * sizeof(int));
 
-  int i;
-  for (i = 0; i < vertices; i++) {
+  for (int i = 0; i < vertices; i++) {
     graph->adjLists[i] = NULL;
     graph->visited[i] = 0;
+    graph->parent[i] = -1;
   }
 
   return graph;
@@ -93,11 +142,11 @@ void addEdge(struct Graph* graph, int src, int dest) {
   graph->adjLists[dest] = newNode;
 }
 
-// Create a queue
 struct queue* createQueue() {
   struct queue* q = malloc(sizeof(struct queue));
   q->front = -1;
   q->rear = -1;
+  q->items = malloc(SIZE * sizeof(int)); // Allocate memory for items array
   return q;
 }
 
@@ -125,65 +174,103 @@ void enqueue(struct queue* q, int value) {
 int dequeue(struct queue* q) {
   int item;
   if (isEmpty(q)) {
-
     item = -1;
   } else {
     item = q->items[q->front];
     q->front++;
     if (q->front > q->rear) {
-
       q->front = q->rear = -1;
     }
   }
   return item;
 }
 
+// Print the contents of the queue (for debugging)
+void printQueue(struct queue* q) {
+  if (isEmpty(q)) {
+    printf("Queue is empty\n");
+  } else {
+    printf("Queue contains: ");
+    for (int i = q->front; i <= q->rear; i++) {
+      printf("%d ", q->items[i]);
+    }
+    printf("\n");
+  }
+}
+
 
 int main() {
-
-  int Nodes=-1, Links=-1, TimeSlots=-1, Req=-1;
+  int Nodes = -1, Links = -1, TimeSlots = -1, Req = -1;
   scanf("%d %d %d %d", &Nodes, &Links, &TimeSlots, &Req);
-  struct Graph *graph = createGraph(Nodes);
+  struct Graph* graph = createGraph(Nodes);
 
-  int **carry = (int **)malloc(TimeSlots * sizeof(int *));
+  int** carry = (int**)malloc(TimeSlots * sizeof(int*));
   for (int i = 0; i < TimeSlots; i++) {
-    carry[i] = (int *)malloc(Nodes * sizeof(int));
+    carry[i] = (int*)malloc(Nodes * sizeof(int));
   }
 
-  int **carry_capacity = (int **)malloc(TimeSlots * sizeof(int *));
+  int** carry_capacity = (int**)malloc(TimeSlots * sizeof(int*));
   for (int i = 0; i < TimeSlots; i++) {
-    carry_capacity[i] = (int *)malloc(Nodes * sizeof(int));
+    carry_capacity[i] = (int*)malloc(Nodes * sizeof(int));
   }
 
   int NodeID[Nodes];
   int QuantumMemories[Nodes];
-  for (int i = 0; i < Nodes;i++){
+  for (int i = 0; i < Nodes; i++) {
     scanf("%d %d", &NodeID[i], &QuantumMemories[i]);
   }
 
-  for (int i = 0; i < TimeSlots; i++)
-  {
-    for (int k = 0; k < Nodes; k++)
-    {
+  for (int i = 0; i < TimeSlots; i++) {
+    for (int k = 0; k < Nodes; k++) {
       carry[i][k] = 0;
     }
   }
-  for(int i=0;i<TimeSlots;i++){
-    for (int k = 0; k < Nodes;k++){
+
+  for (int i = 0; i < TimeSlots; i++) {
+    for (int k = 0; k < Nodes; k++) {
       carry_capacity[i][k] = QuantumMemories[k];
     }
   }
 
-  addEdge(graph, 0, 1);
-  addEdge(graph, 0, 2);
-  addEdge(graph, 1, 2);
-  addEdge(graph, 1, 4);
-  addEdge(graph, 1, 3);
-  addEdge(graph, 2, 4);
-  addEdge(graph, 3, 4);
+  int LinkEnd1, LinkEnd2, ID;
 
+  for (int i = 0; i < Links; i++) {
+    scanf("%d %d %d", &ID, &LinkEnd1, &LinkEnd2);
+    addEdge(graph, LinkEnd1, LinkEnd2);
+  }
 
-  bfs(graph, 0);
+  int ReqId[Req], ReqSrc[Req], ReqDst[Req];
+  for (int i = 0; i < Req; i++) {
+    scanf("%d %d %d", &ReqId[i], &ReqSrc[i], &ReqDst[i]);
+  }
 
+  // Allocate memory for storing paths and their sizes
+  int** paths = NULL;
+  int pathSizes = 0;
+  // Perform BFS for each query and store paths
+  for (int i = 0; i < Req; i++) {
+    // Reset visited and parent arrays for each query
+    for (int v = 0; v < Nodes; v++) {
+      graph->visited[v] = 0;
+      graph->parent[v] = -1;
+    }
+    bfs(graph, ReqSrc[i], ReqDst[i], &paths, &pathSizes);
+  }
+
+  // Display stored paths
+  for (int i = 0; i < pathSizes; i++) {
+    printf("Path %d: ", i);
+    for (int j = 0; j < Nodes; j++) {
+      if (paths[i][j] == -1) {
+        break;  // Terminate the loop when encountering -1 in the path
+      }
+      printf("%d ", paths[i][j]);
+    }
+    printf("\n");
+    // Free memory for each individual path
+    free(paths[i]);
+  }
+
+  
   return 0;
 }
